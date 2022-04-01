e378f7ac1998b209b39fc6626f494482c9a0d0da
Maverickspb78 <Maverickspb78@gmail.com>
2022-04-01 14:42:16 +0300

    Добавлена возможность ввода в оборот КМ

---
+++ src/main/java/com/andreev/properties/ReaderProperties.java
@@ -0,0 +1,80 @@
+package com.andreev.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class ReaderProperties {
+
+    private final Properties properties = new Properties();
+
+    public Properties readPropertiesFile() throws FileNotFoundException {
+        String PROPERTIESPATH = "C:\\Users\\aait\\Desktop\\test\\property.txt";
+        try {
+            Scanner sc = new Scanner(new FileInputStream(PROPERTIESPATH));
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.startsWith("+++")||line.startsWith("/")) {
+                } else {
+                    if (line.startsWith("pathToFileKM")) {
+                        String[] str = line.split(" ");
+                        properties.setFileKMPath(str[1]);
+                    } else if (line.startsWith("pathToWriteFile")) {
+                        String[] str = line.split(" ");
+                        properties.setPathToWriteFile(str[1]);
+                    } else if (line.startsWith("pathWriteFileO")) {
+                        String[] str = line.split(" ");
+                        properties.setPathToWriteFileO(str[1]);
+                    } else if (line.startsWith("fileName")) {
+                        String[] str = line.split(" ");
+                        properties.setFileName(str[1]);
+                    } else if (line.startsWith("pathInOborot")) {
+                        String[] str = line.split(" ");
+                        properties.setPathInOborot(str[1]);
+                    } else if (line.startsWith("pathOutOborot")) {
+                        String[] str = line.split(" ");
+                        properties.setPathOutOborot(str[1]);
+                    } else if (line.startsWith("dateOfManufacture")) {
+                        String[] str = line.split(" ");
+                        properties.setDateOfManufacture(str[1]);
+                    } else if (line.startsWith("Inn")) {
+                        String[] str = line.split(" ");
+                        properties.setInn(str[1]);
+                    } else if (line.startsWith("codeTNVD")) {
+                        String[] str = line.split(" ");
+                        properties.setCodeTNVD(str[1]);
+                    } else if (line.startsWith("19LV")) {
+                        String[] str = line.split(" ");
+                        properties.setPrice19LV(str[1]);
+                    } else if (line.startsWith("19LF")) {
+                        String[] str = line.split(" ");
+                        properties.setPrice19LF(str[1]);
+                    } else if (line.startsWith("6LV")) {
+                        String[] str = line.split(" ");
+                        properties.setPrice6LV(str[1]);
+                    } else if (line.startsWith("0,5LV")) {
+                        String[] str = line.split(" ");
+                        properties.setPrice05LV(str[1]);
+                    } else if (line.startsWith("numberDoc")) {
+                        String[] str = line.split(" ");
+                        properties.setNumberDoc(str[1]);
+                    } else if (line.startsWith("dataPerDoc")) {
+                        String[] str = line.split(" ");
+                        properties.setDataPerDoc(str[1]);
+                    } else if (line.startsWith("dateOut")) {
+                        String[] str = line.split(" ");
+                        properties.setDataOut(str[1]);
+                    }
+                }
+            }
+        }
+        catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return properties;
+    }
+
+}
---
+++ src/main/java/com/andreev/writers/WriterVO.java
@@ -0,0 +1,83 @@
+package com.andreev.writers;
+
+import com.andreev.properties.Properties;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class WriterVO {
+
+    public void writeVO(Properties properties) throws IOException {
+
+        final File fileSampleVO = new File(properties.getPathInOborot());
+        final File fileKM = new File(properties.getFileKMPath() + "\\" +properties.getFileName());
+        StringBuilder temp = new StringBuilder(70);
+        String[] str = properties.getFileName().split("o");
+        StringBuilder path = new StringBuilder(properties.getPathToWriteFile())
+                .append(str[0])
+                .append("Ввод_в_оборот_")
+                .append(getDateFileName())
+                .append(".csv");
+        FileWriter writer = new FileWriter(String.valueOf(path), false);
+        System.out.println(fileKM);
+        System.out.println("Путь к файлу: " + properties.getFileKMPath() + "\nИмя файла: " + properties.getFileName());
+
+        try{
+            Scanner scannerSample = new Scanner(fileSampleVO);
+            for (int i = 0; i < 4; i++){
+                String line = scannerSample.nextLine();
+                writer.write(line.replace("Inn", properties.getInn()).replace("Date", properties.getDateOfManufacture()) + "\n");
+            }
+
+            scannerSample.close();
+            Scanner scannerKM = new Scanner(fileKM);
+            int count = 0;
+            while(scannerKM.hasNextLine()) {
+                String line = scannerKM.nextLine();
+                if (scannerKM.hasNextLine()) {
+                    writer.write(String.valueOf(temp.append("\"")
+                            .append(line.substring(0, 31).replace("\"", "\"\""))
+                            .append("\"")
+                            .append(",,")
+                            .append(properties.getDateOfManufacture())
+                            .append(",")
+                            .append(properties.getCodeTNVD())
+                            .append(",,,,,")
+                            .append("\n")
+                    ));
+                    count ++;
+                    temp.delete(0, temp.length());
+                } else {
+                    writer.write(String.valueOf(temp.append("\"")
+                            .append(line.substring(0, 31).replace("\"", "\"\""))
+                            .append("\"")
+                            .append(",,")
+                            .append(properties.getDateOfManufacture())
+                            .append(",")
+                            .append(properties.getCodeTNVD())
+                            .append(",,,,,")
+                    ));
+                    writer.flush();
+                    writer.close();
+                    System.out.println("обработано: " + ++count + " кодов маркировки");
+
+                }
+            }
+            scannerKM.close();
+        }
+        catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private String getDateFileName (){
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date();
+        return format.format(date);
+    }
+}
