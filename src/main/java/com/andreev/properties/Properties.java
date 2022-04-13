ee3f0b5eb392d76c53b0811189878794794a2844
Maverickspb78 <Maverickspb78@gmail.com>
2022-04-13 11:39:12 +0300

    clean

--- src/main/java/com/andreev/properties/Properties.java
+++ src/main/java/com/andreev/properties/Properties.java
@@ -2,73 +2,15 @@ package com.andreev.properties;
 
 public class Properties implements Prop{
 
-//    private String pathToWriteFile;
-//    private String pathToWriteFileO;
-//    private String fileKMPath;
-//    private String fileName;
-//    private String pathInOborot;
-//    private String pathOutOborot;
     private String dateOfManufacture;
     private String inn;
     private String codeTNVD;
-//    private String price19LV;
-//    private String price19LF;
-//    private String price6LV;
-//    private String price05LV;
     private String numberDoc;
     private String dataPerDoc;
     private String dataOut;
 
     public Properties() {
     }
-
-//    public String getPathToWriteFile() {
-//        return pathToWriteFile;
-//    }
-//
-//    public void setPathToWriteFile(String pathToWriteFile) {
-//        this.pathToWriteFile = pathToWriteFile;
-//    }
-//
-//    public String getPathToWriteFileO() {
-//        return pathToWriteFileO;
-//    }
-//
-//    public void setPathToWriteFileO(String pathToWriteFileO) {
-//        this.pathToWriteFileO = pathToWriteFileO;
-//    }
-//
-//    public String getFileKMPath() {
-//        return fileKMPath;
-//    }
-//
-//    public void setFileKMPath(String fileKMPath) {
-//        this.fileKMPath = fileKMPath;
-//    }
-//
-//    public String getFileName() {
-//        return fileName;
-//    }
-//
-//    public void setFileName(String fileName) {
-//        this.fileName = fileName;
-//    }
-//
-//    public String getPathInOborot() {
-//        return pathInOborot;
-//    }
-//
-//    public void setPathInOborot(String pathInOborot) {
-//        this.pathInOborot = pathInOborot;
-//    }
-//
-//    public String getPathOutOborot() {
-//        return pathOutOborot;
-//    }
-//
-//    public void setPathOutOborot(String pathOutOborot) {
-//        this.pathOutOborot = pathOutOborot;
-//    }
 
     public String getDateOfManufacture() {
         return dateOfManufacture;
@@ -93,38 +35,6 @@ public class Properties implements Prop{
     public void setCodeTNVD(String codeTNVD) {
         this.codeTNVD = codeTNVD;
     }
-
-//    public String getPrice19LV() {
-//        return price19LV;
-//    }
-//
-//    public void setPrice19LV(String price19LV) {
-//        this.price19LV = price19LV;
-//    }
-//
-//    public String getPrice19LF() {
-//        return price19LF;
-//    }
-//
-//    public void setPrice19LF(String price19LF) {
-//        this.price19LF = price19LF;
-//    }
-//
-//    public String getPrice6LV() {
-//        return price6LV;
-//    }
-//
-//    public void setPrice6LV(String price6LV) {
-//        this.price6LV = price6LV;
-//    }
-//
-//    public String getPrice05LV() {
-//        return price05LV;
-//    }
-//
-//    public void setPrice05LV(String price05LV) {
-//        this.price05LV = price05LV;
-//    }
 
     public String getNumberDoc() {
         return numberDoc;
